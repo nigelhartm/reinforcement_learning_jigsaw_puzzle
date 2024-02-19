d06b3b0f9177c956a926572a6477703e6bbf352b
Nigel Hartman <127732792+nigelhartm@users.noreply.github.com>
2024-02-19 10:44:17 +0000

    Done

--- new_2024/dqn.py
+++ new_2024/dqn.py
@@ -55,7 +55,7 @@ def init_weights(m):
         torch.nn.init.uniform_(m.weight, -0.01, 0.01)
         m.bias.data.fill_(0.01)
 
-def train(model, start):
+def train(model):
     # Statistics
     solved_cnt = 0
     stats_reward = 0
@@ -169,27 +169,97 @@ def train(model, start):
             solved_cnt += 1
         global_reward += reward
         wandb.log({"iteration": iteration, "epsilon": epsilon, "reward": reward.numpy()[0][0], "qmax": np.max(output.cpu().detach().numpy()), "solved": solved_cnt, "solved_per_iteration": solved_cnt/iteration, "reward_per_iteration": global_reward/iteration, "time_for_iteration": time.time()-iter_start_time})
+    torch.save(model, "pretrained_model/current_model_final.pth")
+
+def test(model):
+    iteration = 0
+    # Iterate training
+    while iteration <= 1:
+        # Init Game
+        end_reward = 0
+        game_state = jigsaw_game()
+        init_action = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        state_reward = game_state.get_state(init_action)
+        reward = state_reward[1]
+        state= torch.from_numpy(state_reward[0].astype(np.float32)).unsqueeze(0)
+        step = state_reward[3]
+        finished = state_reward[2]
+        """
+        # Play until solved or to much steps
+        while finished == False:
+            state=state.unsqueeze(0)
+            if torch.cuda.is_available():
+                state = state.cuda()
+            output = model(state)[0]
+            mask = game_state.getMask().cuda()
+            output = torch.sub(output, mask)
+            action = torch.zeros([model.number_of_actions], dtype=torch.float32)
+            if torch.cuda.is_available():
+                action = action.cuda()
+            random_action = random.random() <= epsilon
+            mask_copy = mask.cpu()
+            mask_copy = mask_copy.numpy()
+            action_space = np.sum(mask_copy == 0)
+            rand_action = random.randint(1, action_space)
+            cond = mask_copy == 1
+            counts = np.cumsum(cond)
+            idx = np.searchsorted(counts, rand_action)-1
+            rand_action_new= np.zeros(1)
+            rand_action_new[0] = idx
+            rand_action_new = torch.from_numpy(rand_action_new)
+            rand_action_new = rand_action_new.type(torch.int)
+            action_index = [rand_action_new
+                            if random_action
+                            else torch.argmax(output)][0]
+            if torch.cuda.is_available():
+                action_index = action_index.cuda()
+            action[action_index] = 1
+            action = action.cpu()
+            state_reward = game_state.get_state(action)
+            state_1 = torch.from_numpy(state_reward[0].astype(np.float32)).unsqueeze(0)
+            reward = state_reward[1]
+            finished = state_reward[2]
+            step = state_reward[3]
+            action = action.unsqueeze(0)
+            reward = torch.from_numpy(np.array([reward], dtype=np.float32)).unsqueeze(0)
+            end_reward = reward
+            list_state.append(state)
+            list_action.append(action)
+            list_state1.append(state_1)
+            state = state_1
+        """
 
 # Main function
 def main():
-    # start a new wandb run to track this script
-    wandb.init(
-        project="jigsaw_dqn",
-        config={
-        "test": "jigsaw",
-        }
-    )
-    if not os.path.exists('pretrained_model/'):
-        os.mkdir('pretrained_model/')
-    model = NeuralNetwork()
-    if torch.cuda.is_available():
-        model = model.cuda()
+    if mode == 'train':
+        # start a new wandb run to track this script
+        wandb.init(
+            project="jigsaw_dqn",
+            config={
+            "test": "jigsaw",
+            }
+        )
+        if not os.path.exists('pretrained_model/'):
+            os.mkdir('pretrained_model/')
+        model = NeuralNetwork()
+        if torch.cuda.is_available():
+            model = model.cuda()
+        else:
+            sys.exit("No cuda gpu available.")
+        model.apply(init_weights)
+        train(model)
+        wandb.finish()
+    elif mode == 'test':
+        # Model class must be defined somewhere before
+        model = torch.load("pretrained_model/current_model_final.pth")
+        if torch.cuda.is_available():
+            model = model.cuda()
+        else:
+            sys.exit("No cuda gpu available.")
+        model.eval()
+        test(model)
     else:
-        sys.exit("No cuda gpu available.")
-    model.apply(init_weights)
-    start = time.time()
-    train(model, start)
-    wandb.finish()
+        sys.exit("First parameter (mode) does not exist. Possible: train or test")
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
